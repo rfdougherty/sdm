42025248be62449a798f11be13924e0dc87612d9
Renzo Frigato <rfrigato@stanford.edu>
2014-12-08 16:30:03 -0800

    add watcher in the login controller
    fixes #3
    It should remove login inconsistencies

--- app/components/authentication/nimsAuthController.js
+++ app/components/authentication/nimsAuthController.js
@@ -30,13 +30,24 @@ nimsAuthControllers.controller('LoginController', [
             return sdmUserManager.getAuthData();
         };
 
+
+        $scope.$watch(function(){
+            var userData = sdmUserManager.getAuthData();
+
+            return userData?userData.logged_in:null;
+        }, function(newVal, oldVal){
+            if (typeof newVal !== 'undefined') {
+                $scope.logged_in = newVal;
+            }
+        });
+
         var authData = sdmUserManager.getAuthData();
         if (typeof authData !== 'undefined') {
             sdmUserManager.login(authData.access_token).then(
-                function(newAuthData){
-                    console.log('newAuthData', newAuthData);
-                    angular.extend($scope, newAuthData);
-                    $scope.emailHash = $scope.email_hash;
-                });
+            function(newAuthData){
+                console.log('newAuthData', newAuthData);
+                angular.extend($scope, newAuthData);
+                $scope.emailHash = $scope.email_hash;
+            });
         }
     }]);
