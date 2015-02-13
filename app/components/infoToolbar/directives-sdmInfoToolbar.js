9a95e72f3970be35fc69a9b2a65774e9b5d4efef
Renzo Frigato <rfrigato@stanford.edu>
2015-02-12 16:28:22 -0800

    add confirm button to delete collections

--- app/components/infoToolbar/directives-sdmInfoToolbar.js
+++ app/components/infoToolbar/directives-sdmInfoToolbar.js
@@ -4,8 +4,8 @@
     angular.module('sdm.infoToolbar.directives.sdmInfoToolbar',
             ['sdm.APIServices.services.sdmCollectionsInterface',
              'sdm.main.services.sdmViewManager'])
-        .directive('sdmInfoToolbar', [ '$location', 'sdmCollectionsInterface', 'sdmViewManager',
-            function($location, sdmCollectionsInterface, sdmViewManager) {
+        .directive('sdmInfoToolbar', [ '$location', '$compile', 'sdmCollectionsInterface', 'sdmViewManager',
+            function($location, $compile, sdmCollectionsInterface, sdmViewManager) {
                 return {
                     restrict: 'E',
                     scope: false,
@@ -14,12 +14,29 @@
                     controller: function(){},
                     controllerAs: 'sdmTBController',
                     link: function($scope, $element, $attrs, sdmTBController){
-
                         $scope.data = $scope.$parent.$parent.data;
                         console.log($scope.data);
                         $scope.hideToolbar = $scope.$parent.hidePopover;
                         console.log($scope);
                         sdmTBController.location = $location.path();
+                        var appended;
+                        sdmTBController.createDeleteButton = function(){
+                            if ($scope.data.level.name === 'collections') {
+                                if (appended) {
+                                    appended.remove();
+                                    $scope.$parent.dialogStyle.width = '64px';
+                                    appended = null;
+                                } else {
+                                    var button = '<input type="button"' +
+                                        ' id="info-toolbar-delete" class="btn btn-default btn-xs btn-danger"' +
+                                        ' ng-click="sdmTBController.delete($event)"' +
+                                        ' value="Delete"></input>';
+                                    appended = $compile(button)($scope);
+                                    $element.append(appended);
+                                    $scope.$parent.dialogStyle.width = '109px';
+                                }
+                            }
+                        };
                         sdmTBController.delete = function(){
                             if ($scope.data.level.name === 'collections') {
                                 sdmCollectionsInterface.deleteCollection($scope.data.id).then(function(){
@@ -27,7 +44,7 @@
                                 });
                             }
                             $scope.hideToolbar(null, 0);
-                        }
+                        };
                     }
                 }
             }
