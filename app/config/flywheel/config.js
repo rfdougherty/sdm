052c7f18c70206214b66712a85ec4a9245501600
Gunnar Schaefer <gsfr@stanford.edu>
2015-03-21 10:54:59 -0700

    add flywheel color config

---
+++ app/config/flywheel/config.js
@@ -0,0 +1,5 @@
+var BASE_URL = 'https://demo.flywheel.io/api/';
+var VERIFICATION_ENDPOINT  = 'https://www.googleapis.com/oauth2/v1/tokeninfo';
+var AUTHENTICATION_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth';
+var AUTHENTICATION_PROVIDER = 'Google';
+var CLIENT_ID = '528837662697-8ga3nnke42tl1hah312f7hddtl7p2uhe.apps.googleusercontent.com';
