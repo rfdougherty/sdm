7318a20fc36ec59471f1d6a692ead1cbe24effa2
Renzo Frigato <rfrigato@stanford.edu>
2015-04-07 12:46:28 -0700

    change client_id for default and flywheel config

--- app/config/default/config.js
+++ app/config/default/config.js
@@ -2,4 +2,4 @@ var BASE_URL = 'https://demo.scitran.io/api/';
 var VERIFICATION_ENDPOINT  = 'https://www.googleapis.com/oauth2/v1/tokeninfo';
 var AUTHENTICATION_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth';
 var AUTHENTICATION_PROVIDER = 'Google';
-var CLIENT_ID = '1052740023071-ilsnog03s885vmjeb9l9ppsplsadnfa1.apps.googleusercontent.com';
+var CLIENT_ID = '1052740023071-n20pk8h5uepdua3r8971pc6jrf25lvee.apps.googleusercontent.com';
--- app/config/flywheel/config.js
+++ app/config/flywheel/config.js
@@ -2,4 +2,4 @@ var BASE_URL = 'https://demo.flywheel.io/api/';
 var VERIFICATION_ENDPOINT  = 'https://www.googleapis.com/oauth2/v1/tokeninfo';
 var AUTHENTICATION_ENDPOINT = 'https://accounts.google.com/o/oauth2/auth';
 var AUTHENTICATION_PROVIDER = 'Google';
-var CLIENT_ID = '528837662697-8ga3nnke42tl1hah312f7hddtl7p2uhe.apps.googleusercontent.com';
+var CLIENT_ID = '1052740023071-n20pk8h5uepdua3r8971pc6jrf25lvee.apps.googleusercontent.com';
